861f6650232a5228a3f10806de1f9cd8c166c313
Arnav Eluri <arnav.eluri@gmail.com>
2026-07-12 21:06:27 +0530

    added mermaid diagrams in the design flow module

--- dist/assets/channel-dt6H-4BR.js
+++
@@ -1,1 +0,0 @@
-import{ag as o,ah as n}from"./index--B8hHZUA.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
--- dist/assets/swimlanesDiagram-G3AALYLV-CN43I0Sa.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-23GEKE2U-DVsA_T69.js";import{_ as a}from"./index--B8hHZUA.js";import"./chunk-5VM5RSS4-B7Gz6frK.js";import"./chunk-XXDRQBXY-BDPVSQX_.js";import"./chunk-VR4S4FIN-BVjZQmZr.js";import"./chunk-32BRIVSS-GVF9U8ad.js";import"./channel-dt6H-4BR.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
